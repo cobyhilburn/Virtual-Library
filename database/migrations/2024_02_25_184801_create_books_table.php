f24363a7ac38ba3bd60cafe605452f76ec92cf5e
cobyhilburn <cobyhilburn7@gmail.com>
2024-02-25 13:08:59 -0600

    Inital commit

---
+++ database/migrations/2024_02_25_184801_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained(table: 'users')
+                ->cascadeOnDelete();
+            $table->string('title');
+            $table->string('author');
+            $table->string('category');
+            $table->string('thumbnail')->nullable();
+            $table->integer('page_count');
+            $table->string('print_type');
+            $table->date('published_date');
+            $table->string('isbn');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
